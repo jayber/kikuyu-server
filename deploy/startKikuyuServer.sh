fce244f7790d93358b21926dad5db919926a502b
veretie <edvinas.veretinskas@gmail.com>
2013-07-02 16:32:26 +0100

    Changes:  * Template start server script file updated with kikuyu app WS URL example and port example;  * Deploy script changed slightly to automatically create remote deployment directory if it does not exist yet;  * README file updated with set-up and deployment comments

--- deploy/startKikuyuServer.sh
+++ deploy/startKikuyuServer.sh
@@ -1,2 +1,2 @@
 . /apps1/releases/ant-setup.sh
-nohup ./start -DconfigServerContext=localhost:9001 &
+nohup ./start -DconfigServerContext=d37-infra.dev.practicallaw.com:8080/kikuyu -Dhttp.port=9037 &
